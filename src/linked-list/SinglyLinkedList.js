4aabe7064c0704474b158359cc754761b9d7f054
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-04-21 19:01:01 +0900

    update singly-linked-list

--- src/linked-list/SinglyLinkedList.js
+++ src/linked-list/SinglyLinkedList.js
@@ -6,21 +6,44 @@ class Node {
 }
 
 class SinglyLinkedList {
-  constructor(node) {
-    this.head = node;
+  constructor(arr) {
+    this.head = arr.length > 0 ? new Node(arr[0]) : new Node(null);
+
+    let currentNode = this.head;
+    for (let i = 1; i < arr.length; i++) {
+      currentNode.next = new Node(arr[i]);
+      currentNode = currentNode.next;
+    }
+  }
+
+  at(index) {
+    let currentNode = this.head;
+
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+      if (currentNode === null) return null;
+    }
+
+    return currentNode;
+  }
+
+  findNode(key) {
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.data === key) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
   }
 }
 
-const node1 = new Node(3);
-const node2 = new Node(37);
-const node3 = new Node(18);
 
-const numList = new SinglyLinkedList(node1);
-numList.head.next = node2;
-numList.head.next.next = node3;
-
-let currentNode = numList.head;
-while(currentNode !== null) {
-  console.log(currentNode.data);
-  currentNode = currentNode.next;
-}
+const singlyLinkedList = new SinglyLinkedList([35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555]);
+console.log(singlyLinkedList.at(3).data); // 67
+console.log(singlyLinkedList.at(13)); // null
+console.log(singlyLinkedList.findNode(67)); // Node { data: 67, next: Node { data: 86, next: Node } }
+console.log(singlyLinkedList.findNode(14)); // null
