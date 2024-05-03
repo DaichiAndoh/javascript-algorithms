eb049ba91a0cd80898cbfd6ab9f330e18cb00246
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-05-03 19:46:12 +0900

    add bogo-sort

---
+++ src/algorithms/sort/BogoSort.js
@@ -0,0 +1,27 @@
+class BogoSort {
+  static sort(arr) {
+    function isSorted(arr) {
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function shuffle(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        const j = Math.floor(Math.random() * (arr.length));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+
+    while (!isSorted(arr)) {
+      shuffle(arr);
+    }
+    return arr;
+  }
+}
+
+const arr = [2, 4, 3, 1, 5];
+console.log(BogoSort.sort(arr));
