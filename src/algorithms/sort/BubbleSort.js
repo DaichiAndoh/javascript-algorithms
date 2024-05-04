e18cbcc545cd25bbd2b751da82af94762081645b
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-05-04 18:40:49 +0900

    add bubble-sort

---
+++ src/algorithms/sort/BubbleSort.js
@@ -0,0 +1,16 @@
+class BubbleSort {
+  static sort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  }
+}
+
+const arr = [2, 4, 3, 1, 5];
+console.log(BubbleSort.sort(arr));
