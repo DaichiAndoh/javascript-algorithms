0d736f921bd67ce9449e35a43597b64b88f3f81a
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-06-07 22:44:49 +0900

    add problem of array

--- src/problems/array/index.js
+++ src/problems/array/index.js
@@ -40,3 +40,39 @@ function arrToIntPlusOne(arr) {
 console.log(arrToIntPlusOne([1, 2, 3])); // 124
 console.log(arrToIntPlusOne([3, 8, 9, 9])); // 3900
 console.log(arrToIntPlusOne([9, 9, 9, 9, 9, 9])); // 1000000
+
+
+/**
+ * 文字列配列の配列を返す
+ * この配列をそれぞれjoinして文字列として出力すると、元の文字列がスネーク状で出力される
+ * @param {string[]} arr 文字列配列
+ * @returns 文字列配列の配列
+ */
+function snakeStringV1(arr) {
+  const result = [[], [], []];
+  const resultIndexes = new Set([0, 1, 2]);
+  let insertIndex = 1;
+
+  for (const i in arr) {
+    if (i % 4 === 1) {
+      insertIndex = 0;
+    } else if (i % 2 === 0) {
+      insertIndex = 1;
+    } else if (i % 4 === 3) {
+      insertIndex = 2;
+    }
+
+    result[insertIndex].push(arr[i]);
+    resultIndexes.forEach(_i => {
+      if (_i !== insertIndex) {
+        result[_i].push(' ');
+      }
+    });
+  }
+
+  return result;
+}
+
+for (const arr of snakeStringV1('0123456789')) {
+  console.log(arr.join(''));
+}
