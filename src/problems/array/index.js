eed95ec8fbdc46ca8b6588ef49f201e71e392b1a
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-06-08 22:59:32 +0900

    add problem of array

--- src/problems/array/index.js
+++ src/problems/array/index.js
@@ -46,7 +46,7 @@ console.log(arrToIntPlusOne([9, 9, 9, 9, 9, 9])); // 1000000
  * 文字列配列の配列を返す
  * この配列をそれぞれjoinして文字列として出力すると、元の文字列がスネーク状で出力される
  * @param {string[]} arr 文字列配列
- * @returns 文字列配列の配列
+ * @returns {string[][]} 文字列配列の配列
  */
 function snakeStringV1(arr) {
   const result = [[], [], []];
@@ -74,5 +74,43 @@ function snakeStringV1(arr) {
 }
 
 for (const arr of snakeStringV1('0123456789')) {
+  console.log(arr.join(''));
+}
+
+
+/**
+ * 文字列配列の配列を返す
+ * この配列をそれぞれjoinして文字列として出力すると、元の文字列がdepth分の幅を持つスネーク状で出力される
+ * @param {string[]} arr 文字列配列
+ * @param {number} depth 幅
+ * @returns {string[][]} 文字列配列の配列
+ */
+function snakeStringV2(arr, depth) {
+  const result = new Array(depth).fill().map(() => []);
+  const resultIndexes = new Set([...Array(depth).keys()]);
+  let insertIndex = Math.floor(depth / 2);
+
+  const pos = (i) => i + 1;
+  const neg = (i) => i - 1;
+
+  let op = neg;
+
+  for (const s of arr) {
+    result[insertIndex].push(s);
+    resultIndexes.forEach(_i => {
+      if (_i !== insertIndex) {
+        result[_i].push(' ');
+      }
+    });
+
+    if (insertIndex <= 0) op = pos;
+    if (insertIndex >= depth - 1) op = neg;
+    insertIndex = op(insertIndex);
+  }
+
+  return result;
+}
+
+for (const arr of snakeStringV2('abcdefghijklmnopqrstuvwxyz', 5)) {
   console.log(arr.join(''));
 }
