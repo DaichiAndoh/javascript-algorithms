2d54c6f7a78758f758854f0a8c23988434c56e27
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-06-17 08:57:43 +0900

    add problem of stack

--- src/problems/stack/index.js
+++ src/problems/stack/index.js
@@ -30,3 +30,44 @@ console.log(validateJson(JSON.stringify({
   b: [1, 2, 3],
   c: {d: 1},
 }) + '{'));
+
+
+/**
+ * phoneNumberから生成できる全てのニーモニックの配列を返す
+ * @param {string} phoneNumber 電話番号を示す文字列
+ * @returns {string[]} 電話番号から生成できる全てのニーモニックの配列
+ */
+function getPhoneMnemoics(phoneNumber) {
+  const NUM_ALPHABET_MAP = {
+    0: '+',
+    1: '@',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ',
+  };
+
+  const phoneNumberArr = phoneNumber.replace(/-/g, '').split('').map(n => parseInt(n));
+  const candidate = [];
+  const stack = [''];
+
+  while (stack.length) {
+    const alphabet = stack.pop();
+
+    if (alphabet.length === phoneNumberArr.length) {
+      candidate.push(alphabet);
+    } else {
+      for (const char of NUM_ALPHABET_MAP[phoneNumberArr[alphabet.length]]) {
+        stack.push(alphabet + char);
+      }
+    }
+  }
+
+  return candidate;
+}
+
+console.log(getPhoneMnemoics('1-2-3'));
