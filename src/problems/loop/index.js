1ac833a18853b99a2a17ff3e7a926776dbddcad3
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-06-20 19:59:31 +0900

    add problem of loop

--- src/problems/loop/index.js
+++ src/problems/loop/index.js
@@ -1,7 +1,7 @@
 /**
  * numが素数かどうかの判定を行う
  * @param {number} num 数値
- * @returns numが素数であればtrue, 素数でなければfalse
+ * @returns {boolean} numが素数であればtrue, 素数でなければfalse
  */
 function isPrime(num) {
   if (num <= 1) return false;
@@ -20,3 +20,43 @@ console.log(isPrime(19));
 console.log(isPrime(137));
 console.log(isPrime(120));
 console.log(isPrime(125));
+
+
+/**
+ * matchAnswerNum分のパターン数をもつタクシー数をmaxAnswerNum分返す
+ * @param {number} maxAnswerNum 取得するタクシー数の数
+ * @param {number} matchAnswerNum 取得するタクシー数のパターン数
+ * @returns {string} 結果文字列
+ */
+function getTaxicabNumbers(maxAnswerNum, matchAnswerNum) {
+  const result = {};
+  let gotAnswerCount = 0;
+  let answer = 1;
+
+  while (gotAnswerCount < maxAnswerNum) {
+    let matchAnswerCount = 0;
+    const memo = {};
+
+    const maxParam = Math.pow(answer, 1 / 3);
+    for (let i = 1; i <= maxParam; i++) {
+      for (let j = i; j < maxParam; j++) {
+        if (i ** 3 + j ** 3 === answer) {
+          matchAnswerCount++;
+          if (answer in memo) memo[answer].push([i, j]);
+          else memo[answer] = [[i, j]];
+        }
+      }
+    }
+
+    if (matchAnswerCount === matchAnswerNum) {
+      result[answer] = memo[answer];
+      gotAnswerCount++;
+    }
+
+    answer++;
+  }
+
+  return JSON.stringify(result);
+}
+
+console.log(getTaxicabNumbers(2, 2));
